eb9d19b42a8176f75ccad4f4a67d36aa7303d447
myhyun0002 <a01089417552@gmail.com>
2023-05-18 16:11:14 +0900

    [init] 프로젝트 생성

---
+++ src/main/java/com/example/vikrant/VikrantApplication.java
@@ -0,0 +1,13 @@
+package com.example.vikrant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikrantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikrantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vikrant/VikrantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vikrant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VikrantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
